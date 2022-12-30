f35eba95582da7fbe5e436a6ac1089a5d12eff9c
Omkar Kulkarni <omkarkulkarni@Omkars-MacBook-Pro.local>
2022-12-31 02:53:54 +0530

    initial commit

---
+++ two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dict.keys():
+                return [i, dict[target - nums[i]]]
+            
+            dict[nums[i]] = i
