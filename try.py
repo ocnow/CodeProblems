1bd4338b5aaf539c6231e017c1d81191c8c7e130
ocnow <ockulkarni97@gmail.com>
2023-01-07 00:54:59 +0530

    longest-common-prefix solved

--- try.py
+++ try.py
@@ -1,26 +1,4 @@
-print(3881//1000)
+a = [1,0]
+b = [0,1]
 
-def getRoman(num : int) -> str:
-    a = num//1000
-    b = num%1000
-    
-    c = b//500
-    d = b%500
-
-    e = d//100
-    f = d%100
-
-    g = f//50
-    h = f%50
-
-    i = h//10
-    j = h%10
-
-    k = j//5
-    l = j%5
-
-    m = l
-
-    return (a*'M') + (c* 'D') + (e * 'C') + (g * 'L') + (i * 'X') + (k * 'V') + (m * 'I')
-
-print(getRoman(51))
+print([a[i]|b[i] for i in range(2)])
--- two-sum.py
+++ two-sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         dict = {}
         for i in range(len(nums)):
             if target - nums[i] in dict.keys():
