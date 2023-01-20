b22307415c894b2a505f17d4241d0d722d4c1174
ocnow <ockulkarni97@gmail.com>
2023-01-21 02:28:10 +0530

    corrected

--- plus-one.py
+++ plus-one.py
@@ -1,5 +1,5 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+    def plusOne(self, digits: list[int]) -> list[int]:
         carry = 1
         for i in range(len(digits)-1,-1,-1):
             digits[i] = digits[i] + carry
