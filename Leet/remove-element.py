bc19458a09b1096cc0689cc1147a34438fd47731
ocnow <ockulkarni97@gmail.com>
2023-01-22 23:40:24 +0530

    cleaning

--- Leet/remove-element.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeElement(self, nums: list[int], val: int) -> int:
-        cor_indx = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[cor_indx] = nums[i]
-                cor_indx+=1
-        
-        return cor_indx
--- Leet/two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        dict = {}
-        for i in range(len(nums)):
-            if target - nums[i] in dict.keys():
-                return [i, dict[target - nums[i]]]
-            
-            dict[nums[i]] = i
