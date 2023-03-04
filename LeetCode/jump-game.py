b1c6efbec9a1bce90182126e59df0126ade8915f
ocnow <ockulkarni97@gmail.com>
2023-03-04 22:48:05 +0530

    leet solved!

---
+++ LeetCode/jump-game.py
@@ -0,0 +1,19 @@
+ class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        le = len(nums)
+        current = 0
+        farthest = 0
+        #rng = nums[current]
+
+        indx = 0
+        while indx <= farthest:
+            jmp = nums[indx] + indx
+            if jmp >= le - 1:
+                return True
+            
+            if jmp > farthest:
+                farthest = jmp
+            
+            indx+=1
+        
+        return False
